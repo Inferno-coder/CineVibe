7a18135da77a5d35bc8cf6721cef713b60794ef2
Pradeep <98871228+Inferno-coder@users.noreply.github.com>
2026-03-03 23:12:59 +0530

    Delete frontend directory

--- frontend/src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import locationReducer from "../slicers/locationSlice";
-
-export const store = configureStore({
-  reducer: {
-    location: locationReducer,
-  },
-});
--- frontend/src/assets/district.js
+++
@@ -1,9 +0,0 @@
-export const LOCATIONS = {
-  CHENNAI: "Chennai",
-  COIMBATORE: "Coimbatore",
-  MADURAI: "Madurai",
-  SALEM: "Salem",
-  TIRUCHIRAPPALLI: "Tiruchirappalli",
-  TIRUNELVELI: "Tirunelveli",
-  TIRUPPUR: "Tiruppur",
-};
